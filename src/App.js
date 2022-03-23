cae6ef50e6ffaf99736a3c164aefb830a6caaf6e
Elizabeth_O <lizy1909@yahoo.com>
2022-03-23 23:47:14 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
-import ToDoList from "./Components/TodoList";
+import Todo from "./Components/Todo";
 
-function App() {
+const App = () => {
+  const [tasks, addTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    addTasks([...tasks, input]);
+    setInput("");
+  };
+
+  const deleteHandler = (index) => {
+    let storedTasks = [...tasks];
+    storedTasks.splice(index, 1);
+    addTasks(storedTasks);
+  };
+
   return (
-    <div className="ToDo-app">
-      <ToDoList />
+    <div className="App">
+      <h1>Add Tasks</h1>
+      <form onSubmit={(event) => event.preventDefault()}>
+        <input
+          type="text "
+          value={input}
+          placeholder="add new tasks"
+          onChange={(event) => {
+            setInput(event.target.value);
+          }}
+        ></input>
+        <button onClick={submitHandler}>Add</button>
+
+        {tasks.map((task, index) => (
+          <Todo
+            key={task}
+            index={index}
+            task={task}
+            deleteHandler={deleteHandler}
+          />
+        ))}
+      </form>
     </div>
   );
-}
+};
 
 export default App;
+
